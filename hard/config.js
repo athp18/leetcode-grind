7a283d96ab07c83bb087bd779958ffda60386b0e
athp18 <129908089+athp18@users.noreply.github.com>
2024-06-12 16:08:31 -0700

    Create config.js

---
+++ hard/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  development: {
+    username: 'your_username',
+    password: 'your_password',
+    database: 'collaborative_task_manager',
+    host: '127.0.0.1',
+    dialect: 'postgres',
+  },
+  test: {
+    username: 'your_username',
+    password: 'your_password',
+    database: 'database_test',
+    host: '127.0.0.1',
+    dialect: 'postgres',
+  },
+  production: {
+    username: 'your_username',
+    password: 'your_password',
+    database: 'database_production',
+    host: '127.0.0.1',
+    dialect: 'postgres',
+  },
+};
