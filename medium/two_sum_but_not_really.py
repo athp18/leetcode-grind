83a4d2bb0de8f4ebee33a17ac7c9b011709e929c
athp18 <129908089+athp18@users.noreply.github.com>
2024-06-21 08:11:49 -0700

    realized i'm sped and i needed a two pointer approach to keep O(1) space complexity

--- medium/two_sum_but_not_really.py
+++ medium/two_sum_but_not_really.py
@@ -1,9 +1,14 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        prevMap = {}
-        for index, value in enumerate(numbers):
-            diff = target - value
-            if diff in prevMap:
-                return [prevMap[diff], index+1]
-            prevMap[value] = index+1
+        pointer_one = 0
+        pointer_two = len(numbers) - 1
+        while pointer_one < pointer_two:
+            sum_pointers = numbers[pointer_one] + numbers[pointer_two]
+
+            if sum_pointers == target:
+                return [pointer_one+1, pointer_two+1]
+            elif sum_pointers < target:
+                pointer_one += 1
+            else:
+                pointer_two -= 1
         return
