14117baeb8b571a8f638cf93f82d5c68617355a6
athp18 <129908089+athp18@users.noreply.github.com>
2024-06-23 12:03:52 -0700

    Update is_same_binarytree.py

--- easy/is_same_binarytree.py
+++ easy/is_same_binarytree.py
@@ -1,19 +1,25 @@
 # Definition for a binary tree node.
-# class TreeNode:
+# class TreeNode(object):
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-
-class Solution:
-    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+class Solution(object):
+    def isSameTree(self, p, q):
+        """
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: bool
+        """
         if not p and not q:
             return True
         if not p and q:
             return False
         if not q and p:
             return False
-        if p and q and p.val == q.val:
-            return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
-        else: 
+        if p.val != q.val:
             return False
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+
+# runtime complexity: O(n)
+# space complexity: O(n)
