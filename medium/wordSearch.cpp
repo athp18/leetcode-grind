2685d718bc7c7fc97e6368ec5d5155f6506e5907
athp18 <129908089+athp18@users.noreply.github.com>
2024-06-26 23:12:06 -0700

    Create wordSearch.cpp

---
+++ medium/wordSearch.cpp
@@ -0,0 +1,39 @@
+#include <vector>
+#include <string>
+
+class Solution {
+public:
+    bool exist(std::vector<std::vector<char>>& board, std::string word) {
+        int rows = board.size();
+        int cols = board[0].size();
+        
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols; ++j) {
+                if (backtrack(board, word, i, j, 0)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    
+private:
+    bool backtrack(std::vector<std::vector<char>>& board, const std::string& word, int r, int c, int index) {
+        if (index == word.length()) {
+            return true;
+        }
+        
+        if (r < 0 || r >= board.size() || c < 0 || c >= board[0].size() || board[r][c] != word[index]) {
+            return false;
+        }
+        char temp = board[r][c]; // mark as visited
+        board[r][c] = '#';
+
+        bool result = (backtrack(board, word, r + 1, c, index + 1) ||
+                       backtrack(board, word, r - 1, c, index + 1) ||
+                       backtrack(board, word, r, c + 1, index + 1) ||
+                       backtrack(board, word, r, c - 1, index + 1));
+        board[r][c] = temp;
+        return result;
+    }
+};
