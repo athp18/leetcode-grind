fce12e5dc718dc149dc468de35e72d5ede6add11
athp18 <129908089+athp18@users.noreply.github.com>
2024-07-02 09:07:27 -0700

    Update maxPathSum.py

--- hard/maxPathSum.py
+++ hard/maxPathSum.py
@@ -5,18 +5,21 @@
 #         self.left = left
 #         self.right = right
 
+# O(n) time and space complexit
+
 class Solution:
     def maxPathSum(self, root: Optional[TreeNode]) -> int:
         # guys im fucked
         # blind 75
         res = [root.val]
-        def dfs(root):
+        def dfs(root): ## depth first search
             if not root:
                 return 0
-            leftMax = dfs(root.left)
-            rightMax = dfs(root.right)
-            leftMax = max(leftMax, 0)
-            rightMax = max(rightMax, 0)
-            res[0] = max(res[0], root.val+leftMax+rightMax)
+            leftMax = dfs(root.left) #dfs the left subtree
+            rightMax = dfs(root.right) #dfs the right subtree
+            leftMax = max(leftMax, 0) # identify the max of the left
+            rightMax = max(rightMax, 0) # identify the max of the right
+            res[0] = max(res[0], root.val+leftMax+rightMax) # append the max path sum
             return root.val + max(leftMax, rightMax)
+        dfs(root)
         return res[0]
