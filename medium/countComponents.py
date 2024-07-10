9f878559c09a04e95259904b3a3c26c620659e23
athp18 <129908089+athp18@users.noreply.github.com>
2024-07-09 23:08:14 -0700

    Create countComponents.py

---
+++ medium/countComponents.py
@@ -0,0 +1,23 @@
+# fuck this fucking problem
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        adjacencyMat = [[0 for _ in range(n)] for _ in range(n)]
+        for u, v in edges:
+            adjacencyMat[u][v] = 1
+            adjacencyMat[v][u] = 1
+        visited = [False] * n
+        count = 0
+
+        def dfs(node):
+            visited[node] = True
+            for neighbor, connected in enumerate(adjacencyMat[node]):
+                if connected and not visited[neighbor]:
+                    dfs(neighbor)
+        
+        for node in range(n):
+            if not visited[node]:
+                dfs(node)
+                count += 1
+                
+        return count
+            
