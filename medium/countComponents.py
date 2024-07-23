1a067dbefe4915f830a46e1239219529b10f9370
athp18 <129908089+athp18@users.noreply.github.com>
2024-07-23 08:52:23 -0700

    fixed bug

--- medium/countComponents.py
+++ medium/countComponents.py
@@ -1,4 +1,4 @@
-# fuck this fucking problem
+# fuck this problem
 class Solution:
     def countComponents(self, n: int, edges: List[List[int]]) -> int:
         adjacencyMat = [[0 for _ in range(n)] for _ in range(n)]
@@ -13,7 +13,7 @@ class Solution:
             for neighbor, connected in enumerate(adjacencyMat[node]):
                 if connected and not visited[neighbor]:
                     dfs(neighbor)
-        
+    
         for node in range(n):
             if not visited[node]:
                 dfs(node)
