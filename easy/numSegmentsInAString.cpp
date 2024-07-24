a4a35bb79f3cd35de2dc5f6dabf616836db4b034
athp18 <129908089+athp18@users.noreply.github.com>
2024-07-23 21:01:16 -0700

    Create numSegmentsInAString.cpp

---
+++ easy/numSegmentsInAString.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int countSegments(string s) {
+        int count = 0;
+        bool inSegment = false;
+        
+        for (char c : s) {
+            if (isspace(c)) {
+                inSegment = false;
+            } else {
+                if (!inSegment) {
+                    count++;
+                }
+                inSegment = true;
+            }
+        }
+        
+        return count;
+    }
+};
