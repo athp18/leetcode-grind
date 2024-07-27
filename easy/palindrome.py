f65f61ceadec6b83c7d5d260086f01b2b1f61279
athp18 <129908089+athp18@users.noreply.github.com>
2024-07-26 21:18:17 -0700

    Update palindrome.py

--- easy/palindrome.py
+++ easy/palindrome.py
@@ -7,3 +7,16 @@ class Solution:
                 newlst.append(char)
         return newlst == newlst[::-1]
       # easy peasy
+class Solution1: # better, more optimal solution
+    def isPalindrome(self, s: str) -> bool:
+        l, r = 0, len(s) - 1
+        while l < r:
+            while l < r and not s[l].isalnum():
+                l += 1
+            while l < r and not s[r].isalnum():
+                r -= 1
+            if s[l].lower() != s[r].lower():
+                return False
+            l += 1
+            r -= 1
+        return True
