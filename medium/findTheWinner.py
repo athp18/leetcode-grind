0dd19afe3344349c67d3a41d695ce871a5c2c6e1
athp18 <129908089+athp18@users.noreply.github.com>
2024-07-28 08:48:24 -0700

    Create findTheWinner.py

---
+++ medium/findTheWinner.py
@@ -0,0 +1,9 @@
+class Solution:
+    def findTheWinner(self, n: int, k: int) -> int:
+        lst = list(range(1, n+1))
+        curr = 0
+
+        while len(lst) > 1:
+            curr = (curr + k -1) % len(lst)
+            lst.pop(curr)
+        return lst[0]
