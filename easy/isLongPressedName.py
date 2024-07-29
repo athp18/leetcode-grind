95df00486c83fe8a98047a38489b679bb2591540
athp18 <129908089+athp18@users.noreply.github.com>
2024-07-28 18:42:40 -0700

    Create isLongPressedName.py

---
+++ easy/isLongPressedName.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def isLongPressedName(self, name, typed):
+        """
+        :type name: str
+        :type typed: str
+        :rtype: bool
+        """
+        i, j = 0, 0
+        while j < len(typed):
+            if i < len(name) and name[i] == typed[j]:
+                i += 1  
+            elif j == 0 or typed[j] != typed[j - 1]:
+                return False  
+            j += 1  
+        return i == len(name)  
