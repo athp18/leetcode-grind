a5f47691d1ee011da7c65e5a53abb7ddf3ae77a8
athp18 <129908089+athp18@users.noreply.github.com>
2024-07-30 00:29:35 -0700

    Update findTargetInRotatedArray.py

--- medium/findTargetInRotatedArray.py
+++ medium/findTargetInRotatedArray.py
@@ -19,3 +19,27 @@ class Solution:
                 else:
                     r = mid - 1
         return -1
+        
+class Solution1(object):
+    def search(self, nums, target): # second (more simple imo) implementation that just uses logic
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            mid = (l + r) // 2
+            if nums[mid] == target:
+                return mid
+            if nums[l] <= nums[mid]:
+                if nums[l] <= target <= nums[mid]: # left portion is sorted
+                    r = mid -1
+                else:
+                    l = mid+ 1
+            else:
+                if nums[mid] < target <= nums[r]: 
+                    l = mid + 1 # right half is sorted
+                else:
+                    r = mid -1
+        return -1
