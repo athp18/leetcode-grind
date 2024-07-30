bc8a34258ac8276d3fcecdb6445574cee15c5f00
athp18 <129908089+athp18@users.noreply.github.com>
2024-07-30 00:56:11 -0700

    Create searchRangeBinSearch.py

---
+++ medium/searchRangeBinSearch.py
@@ -0,0 +1,36 @@
+class Solution(object):
+    def searchRange(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        def binary_search_left(nums, target):
+            left, right = 0, len(nums) - 1
+            while left <= right:
+                mid = left + (right - left) // 2
+                if nums[mid] < target:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+            return left if left < len(nums) and nums[left] == target else -1
+
+        def binary_search_right(nums, target):
+            left, right = 0, len(nums) - 1
+            while left <= right:
+                mid = left + (right - left) // 2
+                if nums[mid] <= target:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+            return right if right >= 0 and nums[right] == target else -1
+
+        if not nums:
+            return [-1, -1]
+
+        left_index = binary_search_left(nums, target)
+        if left_index == -1:
+            return [-1, -1]
+
+        right_index = binary_search_right(nums, target)
+        return [left_index, right_index]
