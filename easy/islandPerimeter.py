a4cf7b0f1254b4488422d5999fd955a929e4cab3
athp18 <129908089+athp18@users.noreply.github.com>
2024-08-04 13:19:25 -0700

    Create islandPerimeter.py

---
+++ easy/islandPerimeter.py
@@ -0,0 +1,12 @@
+class Solution:
+    def islandPerimeter(self, grid: List[List[int]]) -> int:
+        p, r, c = 0, len(grid), len(grid[0])
+        for i in range(r):
+            for j in range(c):
+                if grid[i][j] == 1:
+                    p += 4
+                    if i > 0 and grid[i-1][j] == 1:
+                        p -= 2
+                    if j > 0 and grid[i][j-1] == 1:
+                        p -= 2
+        return p
