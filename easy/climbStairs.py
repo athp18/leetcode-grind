b102643ba5f0dd6ac00cce2a4be9ff602b61ea29
athp18 <129908089+athp18@users.noreply.github.com>
2024-08-04 11:54:20 -0700

    Update climbStairs.py

--- easy/climbStairs.py
+++ easy/climbStairs.py
@@ -11,4 +11,18 @@ class Solution:
         for i in range(2, n+1):
             f.append(f[i-1]+f[i-2])
         return f[n]
-        # runtime complexity O(n), space time complexity O(n)
+        # runtime complexity O(n), space complexity O(n)
+
+class Solution:
+    def climbStairs(self, n: int) -> int: # slightly more efficient solution: instead of having an array of n elements, we just use temp vars
+        # to update
+        if n < 0:
+            return -1
+        if n == 0:
+            return 0
+        a, b = 1, 1
+        for i in range(2,n+1):
+            temp = a + b
+            b = a
+            a = temp
+        return a
