c46d5e3fa7c32c335d9e10c101a48248a4303dfa
athp18 <129908089+athp18@users.noreply.github.com>
2024-08-10 18:43:15 -0700

    Update firstPalindrome.py

--- firstPalindrome.py
+++ firstPalindrome.py
@@ -1,11 +1,1 @@
-class Solution: 
-    def firstPalindrome(self, words: List[str]) -> str:
-        def isPalindrome(s):
-            l = len(s) # divide string length by 2 and check if the first half of the string and reversed second half are equivalent. O(n) time complexity
-            return s[:l // 2] == s[(l + 1) // 2:][::-1]
 
-        for word in words: # O(n) time complexity
-            if isPalindrome(word):
-                return word
-
-        return "" # O(n) time complexity, O(1) space complexity
