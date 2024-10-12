5ff330b28c6af37d24c5f16218202fadc647f168
athp18 <129908089+athp18@users.noreply.github.com>
2024-10-11 21:01:41 -0700

    Create summaryRanges1.py

---
+++ easy/summaryRanges1.py
@@ -0,0 +1,23 @@
+class Solution:
+    def summaryRanges(self, nums: List[int]) -> List[str]: # Recursive approach
+      # I'm not hesitant to use this since the length of nums is guarantee to be < 20
+        res = []
+        self.traverse(nums, 0, 1, res)
+        return res
+    
+    def traverse(self, nums: List[int], l: int, r: int, res: List[str]):
+        if l >= len(nums):  # Check if l is out of bounds
+            return
+        
+        # Adjust r to find the end of the range
+        while r < len(nums) and nums[r] - nums[r - 1] == 1:
+            r += 1
+            
+        # Build the range string
+        if r - l > 1:  # range exists
+            res.append(f"{nums[l]}->{nums[r - 1]}")
+        else: #there's a single number
+            res.append(str(nums[l]))
+        
+        # recursive call
+        self.traverse(nums, r, r + 1, res)
