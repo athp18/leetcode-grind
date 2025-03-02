90c56542dc177e9706691cee1398d109d04c8c37
athp18 <129908089+athp18@users.noreply.github.com>
2025-03-02 10:28:25 -0800

    Create singleElementInSortedArray.py

---
+++ medium/singleElementInSortedArray.py
@@ -0,0 +1,14 @@
+class Solution:
+    def singleNonDuplicate(self, nums: List[int]) -> int:
+        l, r = 0, len(nums) - 1
+        while l < r:
+            mid = l + (r-l) // 2
+            if mid % 2 != 0:
+                mid -= 1
+            
+            if nums[mid] == nums[mid + 1]:
+                l = mid + 2
+            else:
+                r = mid
+        return nums[l]
+            
